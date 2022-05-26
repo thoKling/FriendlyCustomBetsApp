1c6732036234bd34d401908de83770a5fff4ce93
thomas <klingthomas67@gmail.com>
2022-05-26 16:00:55 +0200

    Init project

---
+++ android/app/src/main/kotlin/com/example/friendly_custom_bets_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.friendly_custom_bets_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:friendly_custom_bets_app/app.dart';
+
+void main() {
+  runApp(const FriendlyCustomBetsApp());
+}
